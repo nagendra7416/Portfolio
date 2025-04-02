c6bba902cdcf55b5be3a83b15d5981283e3575e7
nagendra7416 <nagendrababumekapothula4@gmail.com>
2025-04-02 10:37:31 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import About from './pages/About';
+import Loader from './components/Loader';
 
 export default function App(){
+
+    window.onload = function(){
+        document.querySelector('.loader').style.display = 'none';
+    }
     return (
         <>
-            <div class="loader">
-                <span></span>
-            </div>
+            <Loader />
             <Routes>
                 <Route exact path='/' element={<Home />}></Route>
                 <Route exact path='/about' element={<About />}></Route>
---
+++ src/components/Loader.js
@@ -0,0 +1,9 @@
+export default function Loader(){
+    return (
+        <>
+            <div class="loader">
+                <span></span>
+            </div>
+        </>
+    )
+}
