c91d8827763f9fdb755ccb12cc2649996a20cebf
nagendra7416 <nagendrababumekapothula4@gmail.com>
2025-04-02 11:51:07 +0530

    commit

--- src/pages/Education.js
+++ src/pages/Education.js
@@ -8,7 +8,18 @@ export default function Education(){
             <header className="header">
                 <div className="education-header">
                     <div className="inner">
-                        education
+                        <div className="head">
+                            <h2>My Education</h2>
+                            <label>Steps towards the career</label>
+                        </div>
+
+                        <div className="eduCon">
+                            <div className="eduInner">
+                                <div className="education">.</div>
+                                <div className="education">.</div>
+                                <div className="education">.</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </header>
